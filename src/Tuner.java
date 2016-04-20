6d821afbd50342c74ac8cc223397aac1b852e3a4
Marcin Szmydt <marcin.szmydt@gmail.com>
2016-04-20 16:08:42 +0200

    Tuner

---
+++ src/Tuner.java
@@ -0,0 +1,20 @@
+
+public class Tuner {
+	
+	private boolean am;
+	private boolean fm;
+	private double frequency;
+
+	public void setAm(){
+		am = true;
+	}
+	
+	public void setFm(){
+		fm = true;
+	}
+	
+	public void setFrequency(double frequency){
+		this.frequency=frequency;
+	}
+	
+}
