0e31467ece9fb8a3caacacc7a87942f9604da2af
ArekPatek <patek.arkadiusz@gmail.com>
2016-04-20 16:24:24 +0200

    amp update

--- src/Amplifier.java
+++ src/Amplifier.java
@@ -1,4 +1,19 @@
 
 public class Amplifier {
-
+ public void setStereoSound(){
+	 
+ }
+ public int setVolume(){
+	return 0;
+	 
+ }
+ public void setTuner(Tuner t){
+	 
+ }
+ public void setDvd(DvdPlayer dvd){
+	 
+ }
+ public void setCd(CdPlayer cd){
+	 
+ }
 }
